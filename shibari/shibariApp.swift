d8bca7e67659019491c6a5a2f65f3d7267f5dfa6
HarumaIto <itoharm@gmail.com>
2026-02-24 10:19:33 +0900

    Initial Commit

---
+++ shibari/shibariApp.swift
@@ -0,0 +1,17 @@
+//
+//  shibariApp.swift
+//  shibari
+//
+//  Created by Haruma Ito on 2026/02/24.
+//
+
+import SwiftUI
+
+@main
+struct shibariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shibariTests/shibariTests.swift
@@ -0,0 +1,17 @@
+//
+//  shibariTests.swift
+//  shibariTests
+//
+//  Created by Haruma Ito on 2026/02/24.
+//
+
+import Testing
+@testable import shibari
+
+struct shibariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
