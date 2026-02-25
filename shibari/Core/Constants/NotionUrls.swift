fd68f2da405dd6791c7d69d88824174e6c198400
HarumaIto <itoharm@gmail.com>
2026-02-26 01:12:18 +0900

    add: 利用規約とプライバシーポリシーのURLを追加

---
+++ shibari/Core/Constants/NotionUrls.swift
@@ -0,0 +1,2 @@
+let termsUrl = "https://www.notion.so/jettharumaki/Shibari-3126816c76a18093bc85f29f8c706bd1?source=copy_link"
+let privacyUrl = "https://www.notion.so/jettharumaki/Shibari-3126816c76a18015b2b6f232af90360a?source=copy_link"
