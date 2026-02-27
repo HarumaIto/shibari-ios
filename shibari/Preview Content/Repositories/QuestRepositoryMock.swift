a22d748ac655f31d99242413a7fd5a27b8ac18cb
HarumaIto <itoharm@gmail.com>
2026-02-27 23:44:38 +0900

    update: モックデータを充実

--- shibari/Preview Content/Repositories/QuestRepositoryMock.swift
+++ shibari/Preview Content/Repositories/QuestRepositoryMock.swift
@@ -1,6 +1,16 @@
 class QuestRepositoryMock: QuestRepository {
     func getQuests(groupId: String) async throws -> [Quest] {
-        return [] // ダミーの縛り（Quest）配列を返すとリストUIのテストができます
+        return [
+            Quest(
+                id: "mock_quest_id_123",
+                groupId: "mock_group_id_456",
+                title: "Mock quest",
+                type: QuestType.ROUTINE,
+                frequency: QuestFrequency.DAILY,
+                description: "Mock description",
+                threshold: 1
+            )
+        ]
     }
     
     func createQuest(quest: Quest) async throws { }
